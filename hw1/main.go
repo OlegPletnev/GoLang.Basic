a78144c45dce1cce78cf76d5baa41c9a235e932d
OlegPletnev <oleg-pletnev@yandex.ru>
2025-06-28 01:13:42 +0300

    шахматная доска

---
+++ hw1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to OTUS!")
+
+	fmt.Println("The time is", time.Now())
+}
--- main.go
+++ main.go
@@ -2,11 +2,41 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"os"
+	"strconv"
 )
 
-func main() {
-	fmt.Println("Welcome to OTUS!")
+const draw = "#"
+const draw_ = " "
 
-	fmt.Println("The time is", time.Now())
+func main() {
+
+	var sideDigs, sideLetts int
+
+    // Заложена возможность варьирования размера каждой стороны
+	// (с ошибками решил пока не заморачиваться)
+	switch len(os.Args) {
+	case 2:
+		sideLetts, _ = strconv.Atoi(os.Args[1])
+		sideDigs, _ = strconv.Atoi(os.Args[1])
+	case 3:
+		sideLetts, _ = strconv.Atoi(os.Args[1])
+		sideDigs, _ = strconv.Atoi(os.Args[2])
+	default:
+		sideLetts = 8
+		sideDigs = 8
+	}
+
+	// Подстраиваю так, чтобы левая нижняя клетка была всегда черная (как в шахматах)
+	for i := sideDigs; i > 0; i-- {
+		for j := 1; j <= sideLetts; j++ {
+			// Если сумма индексов четная - черная клетка, иначе - белая
+			if (i+j)%2 == 0 {
+				fmt.Print(draw)
+			} else {
+				fmt.Print(draw_)
+			}
+		}
+		fmt.Println()
+	}
 }
